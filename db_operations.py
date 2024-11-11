585bbf541a7d9b1d61b0f627ec9c406ea1603ac1
Andrii Sokha <sokha.andrey.vn@gmail.com>
2024-11-12 00:32:40 +0200

    Backend update

--- db_operations.py
+++ db_operations.py
@@ -1,16 +1,16 @@
 from app import app, db, Item
 
 def delete_item(item_name):
-    """Удаляет товар по имени из базы данных."""
+    """Видаляє товар по імені із БД"""
     with app.app_context():
 
         item = Item.query.filter_by(name=item_name).first()
         if item:
             db.session.delete(item)
             db.session.commit()
-            print(f"Товар '{item_name}' успешно удалён.")
+            print(f"Товар '{item_name}' видалено")
         else:
-            print(f"Товар '{item_name}' не найден.")
+            print(f"Товар '{item_name}' не знайдено.")
 
 if __name__ == '__main__':
     delete_item('Jack Daniel`s Old')
