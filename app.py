5be6553d730f4fcffce65de12e90f8e5a7002ca1
Andrii Sokha <sokha.andrey.vn@gmail.com>
2024-11-13 14:47:18 +0200

    Оновлено базу даних ( додано нові записи для категорій )

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 db = SQLAlchemy(app)
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     items = db.relationship('Item', backref='category', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,14 +31,21 @@ class Item(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
     def __repr__(self):
-        return  self.name
+        return self.name
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    isAdmin = db.Column(db.Boolean, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    isAdmin = db.Column(db.Boolean, default=False, nullable=False)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 @app.route('/')
 def index():
@@ -49,16 +62,61 @@ def categories():
     return render_template('categories.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        username = request.form['new_username']
+        password = request.form['new_password']
+        email = request.form['email']
+        confirm_password = request.form['confirm_password']
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already taken', 'error')
+            return redirect(url_for('signup'))
+
+        if password != confirm_password:
+            flash('Passwords do not match', 'error')
+            return redirect(url_for('signup'))
+
+
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('You are now registered', 'success')
+        return redirect(url_for('login'))
+
     return render_template('singup.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            flash('You are now logged in', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'error')
+            return redirect(url_for('login'))
+
     return render_template('login.html')
 
-@app.route('/category') # category == beer
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    flash('You are now logged out', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/category')  # category == beer
 def category():
     beer_category = Category.query.filter_by(name='Пиво').first()
     if beer_category:
@@ -66,6 +124,7 @@ def category():
     else:
         items = []
     return render_template('category.html', data=items)
+
 
 @app.route('/whiskey')
 def whiskey():
@@ -76,6 +135,7 @@ def whiskey():
         items = []
     return render_template('whiskey.html', data=items)
 
+
 @app.route('/cognac')
 def cognac():
     cognac_category = Category.query.filter_by(name='Коньяк').first()
@@ -84,6 +144,25 @@ def cognac():
     else:
         items = []
     return render_template('cognac.html', data=items)
+
+@app.route('/gin')
+def gin():
+    gin_category = Category.query.filter_by(name='Джин').first()
+    if gin_category:
+        items = Item.query.filter_by(category=gin_category).all()
+    else:
+        items = []
+    return render_template('gin.html', data=items)
+
+@app.route('/wine')
+def wine():
+    wine_category = Category.query.filter_by(name='Вино').first()
+    if wine_category:
+        items = Item.query.filter_by(category=wine_category).all()
+    else:
+        items = []
+    return render_template('wine.html', data=items)
+
 
 if __name__ == '__main__':
     with app.app_context():
--- categoriesadd.py
+++ categoriesadd.py
@@ -8,7 +8,7 @@ def add_categories_and_items():
         db.session.add(whiskey_category)
         db.session.commit()
 
-    # Список товаров для категории "Віскі"
+
     whiskey_items = [
         {'name': 'Jack Daniel`s Old', 'price': 789, 'description_file': 'whiskey_1',
          'image_url': 'static/picture/alcohol/whiskey/jackblack.png', 'isActive': True,
@@ -93,6 +93,152 @@ def add_categories_and_items():
             category=cognac_category
         )
         db.session.add(item)
+
+    beer_category = Category.query.filter_by(name='Пиво').first()
+    if not beer_category:
+        beer_category = Category(name='Пиво')
+        db.session.add(beer_category)
+        db.session.commit()
+
+    beer_items = [
+        {'name': 'Пиво «Оболонь» світле с/п', 'price': 32.90, 'description_file': 'beer_1',
+         'image_url': 'static/picture/alcohol/beer/Obolon.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4.5, 'producer': 'AB InBev'},
+        {'name': 'Stella Artois', 'price': 26.90, 'description_file': 'beer_2',
+         'image_url': 'static/picture/alcohol/beer/stella.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 5, 'producer': 'AB InBev'},
+        {'name': 'Пиво Senor Cartel світле', 'price': 42.50, 'description_file': 'beer_3',
+         'image_url': 'static/picture/alcohol/beer/cartel.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4.5, 'producer': 'Senor Cartel'},
+        {'name': 'Kurpfalz Brau Helles світле', 'price': 83, 'description_file': 'beer_4',
+         'image_url': 'static/picture/alcohol/beer/helles.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Німеччина', 'strength': 5.2, 'producer': 'Kurpfalz Brau'},
+        {'name': 'Пиво Peroni', 'price': 120, 'description_file': 'beer_5',
+         'image_url': 'static/picture/alcohol/beer/peroni.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Італія', 'strength': 4.7, 'producer': 'Peroni Brewery'},
+        {'name': 'Corona Extra', 'price': 669, 'description_file': 'beer_6',
+         'image_url': 'static/picture/alcohol/beer/corona.jpg', 'isActive': True,
+         'quantity': 10, 'geography': 'Мексика', 'strength': 4.5, 'producer': 'Corona'},
+        {'name': 'Закарпатське Оригінальне світле', 'price': 29, 'description_file': 'beer_7',
+         'image_url': 'static/picture/alcohol/beer/Zakarpatske.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4, 'producer': 'ПАТ Оболонь'},
+        {'name': 'Львівське світле фільтроване', 'price': 33, 'description_file': 'beer_8',
+         'image_url': 'static/picture/alcohol/beer/lvivske_light.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4.5, 'producer': 'Львівська пивоварня'},
+        {'name': 'Львівське Лев темне', 'price': 36, 'description_file': 'beer_9',
+         'image_url': 'static/picture/alcohol/beer/lvivske.jpg', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4.7, 'producer': 'Львівська пивоварня'},
+        {'name': 'Чернігівське, світле, фільтроване', 'price': 39, 'description_file': 'beer_10',
+         'image_url': 'static/picture/alcohol/beer/Chernigyvske.jpg', 'isActive': True,
+         'quantity': 10, 'geography': 'Україна', 'strength': 4.6, 'producer': 'Чернігівська броварня'},
+        {'name': 'Warsteiner Brauerei світле', 'price': 60, 'description_file': 'beer_11',
+         'image_url': 'static/picture/alcohol/beer/wastiner.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Німеччина', 'strength': 5.3, 'producer': 'Krombacher Brauerei'},
+        {'name': 'Original Guinness темне фільтроване', 'price': 90, 'description_file': 'beer_12',
+         'image_url': 'static/picture/alcohol/beer/guinness.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Ірландія', 'strength': 4.8, 'producer': 'Guinness & Co'},
+    ]
+
+    for item_data in beer_items:
+        description = get_description_from_file(item_data['description_file'], 'description_beer.txt')
+        item = Item(
+            name=item_data['name'],
+            price=item_data['price'],
+            description=description,
+            image_url=item_data['image_url'],
+            isActive=item_data['isActive'],
+            quantity=item_data['quantity'],
+            geography=item_data['geography'],
+            strength=item_data['strength'],
+            producer=item_data['producer'],
+            category=beer_category
+        )
+        db.session.add(item)
+
+    gin_category = Category.query.filter_by(name='Джин').first()
+    if not gin_category:
+        gin_category = Category(name='Джин')
+        db.session.add(gin_category)
+        db.session.commit()
+
+    gin_items = [
+        {'name': 'Stirling Pink Gin', 'price': 459, 'description_file': 'gin_1',
+         'image_url': 'static/picture/alcohol/gin/stirlingpink.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Нідерланди', 'strength': 37.5, 'producer': 'Stirling'},
+        {'name': 'LoneWolf Gin', 'price': 1290, 'description_file': 'gin_2',
+         'image_url': 'static/picture/alcohol/gin/lonewolf.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Шотландія', 'strength': 40, 'producer': 'BrewDog'},
+        {'name': 'Chocolate Orange Gin', 'price': 1190, 'description_file': 'gin_3',
+         'image_url': 'static/picture/alcohol/gin/chocolateorange.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Великобританія', 'strength': 46, 'producer': 'TBGC'},
+        {'name': 'Bombay Sapphire', 'price': 1190, 'description_file': 'gin_4',
+         'image_url': 'static/picture/alcohol/gin/bombay.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Великобританія', 'strength': 47, 'producer': 'Bombay'},
+        {'name': 'Daucourt New Angouleme', 'price': 1190, 'description_file': 'gin_5',
+         'image_url': 'static/picture/alcohol/gin/daucourt.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Франція', 'strength': 43, 'producer': 'Daucourt Martin Distillery'},
+    ]
+
+    for item_data in gin_items:
+        description = get_description_from_file(item_data['description_file'], 'description_gin.txt')
+        item = Item(
+            name=item_data['name'],
+            price=item_data['price'],
+            description=description,
+            image_url=item_data['image_url'],
+            isActive=item_data['isActive'],
+            quantity=item_data['quantity'],
+            geography=item_data['geography'],
+            strength=item_data['strength'],
+            producer=item_data['producer'],
+            category=gin_category
+        )
+        db.session.add(item)
+
+    wine_category = Category.query.filter_by(name='Вино').first()
+    if not wine_category:
+        wine_category = Category(name='Вино')
+        db.session.add(wine_category)
+        db.session.commit()
+
+    wine_items = [
+        {'name': 'Вино сухе біле Teruzzi Terre Di Tufi IGT', 'price': 1054, 'description_file': 'wine_1',
+         'image_url': 'static/picture/alcohol/wine/terre.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Італія', 'strength': 12.5, 'producer': 'Teruzzi'},
+        {'name': 'Вино Glou Glou, Orange, Fio Wine, бурштинове сухе', 'price': 775, 'description_file': 'wine_2',
+         'image_url': 'static/picture/alcohol/wine/glou.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Німеччина', 'strength': 12.5, 'producer': 'Teruzzi'},
+        {'name': 'Вино сухе червоне Col d’Orcia Poggio al Vento Brunello di Montalcino Riserva 2012', 'price': 6999, 'description_file': 'wine_3',
+         'image_url': 'static/picture/alcohol/wine/col.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Італія', 'strength': 14.5, 'producer': 'Col dOrcia'},
+        {'name': 'Вино сухе червоне Teruzzi Melograni Toscana Rosso IGT', 'price': 1450, 'description_file': 'wine_4',
+         'image_url': 'static/picture/alcohol/wine/melograni.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Італія', 'strength': 12.5, 'producer': 'Teruzzi'},
+        {'name': 'Вино Vilarissa Valley, Douro Tinto, Barao de Vilar, червоне напівсухе', 'price': 375, 'description_file': 'wine_5',
+         'image_url': 'static/picture/alcohol/wine/valley.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Португалія', 'strength': 12.5, 'producer': 'Barao de Vilar'},
+        {'name': 'Вино El Emperador, Merlot, червоне сухе', 'price': 279, 'description_file': 'wine_6',
+         'image_url': 'static/picture/alcohol/wine/el.png', 'isActive': True,
+         'quantity': 10, 'geography': 'Чилі', 'strength': 12.5, 'producer': 'Vina del Nuevo Mundo'},
+    ]
+
+    for item_data in wine_items:
+        description = get_description_from_file(item_data['description_file'], 'description_wine.txt')
+        item = Item(
+            name=item_data['name'],
+            price=item_data['price'],
+            description=description,
+            image_url=item_data['image_url'],
+            isActive=item_data['isActive'],
+            quantity=item_data['quantity'],
+            geography=item_data['geography'],
+            strength=item_data['strength'],
+            producer=item_data['producer'],
+            category=wine_category
+        )
+        db.session.add(item)
+
+
 
     db.session.commit()
 
